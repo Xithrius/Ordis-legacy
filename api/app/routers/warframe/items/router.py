5c5db502d9e1fd816b61aaee5d0b48338079657b
Xithrius <xithrius@gmail.com>
2023-11-19 22:15:14 -0800

    Warframe items get all and get one

--- api/app/routers/warframe/items/router.py
+++ api/app/routers/warframe/items/router.py
@@ -1,14 +1,15 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from httpx import AsyncClient
+from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.dependencies import get_db_session
-from app.database.models.warframe import WarframeMarketOrderModel
+from app.database.models.warframe import WarframeItemModel
 
-from .schemas import SyncResponse
+from .schemas import ItemsSyncResponse, WarframeItemResponse
 
 router = APIRouter()
 
@@ -21,12 +22,12 @@ warframe_market_api = AsyncClient(
 @router.get(
     "/sync",
     response_description="Syncing all items in Warframe with the database",
-    response_model=SyncResponse,
+    response_model=ItemsSyncResponse,
     status_code=status.HTTP_200_OK,
 )
 async def sync_item(
     session: Annotated[AsyncSession, Depends(get_db_session)],
-) -> SyncResponse:
+) -> ItemsSyncResponse:
     r = await warframe_market_api.get("/items")
 
     data = r.json()
@@ -34,10 +35,51 @@ async def sync_item(
     items = data["payload"]["items"]
 
     result = await session.execute(
-        insert(WarframeMarketOrderModel)
-        .on_conflict_do_nothing(index_elements=["id"])
-        .returning(WarframeMarketOrderModel),
+        insert(WarframeItemModel).on_conflict_do_nothing(index_elements=["id"]).returning(WarframeItemModel),
         items,
     )
 
-    return SyncResponse(response=f"Populated cache with {len(result)} item(s).")
+    rows = result.all()
+
+    return ItemsSyncResponse(response=f"Populated cache with {len(rows)} item(s).")
+
+
+@router.get(
+    "/all",
+    response_description="All the items that currently exist",
+    response_model=list[WarframeItemResponse],
+    status_code=status.HTTP_200_OK,
+)
+async def get_all_items(
+    session: Annotated[AsyncSession, Depends(get_db_session)],
+    limit: int | None = None,
+    offset: int | None = None,
+) -> list[WarframeItemModel]:
+    stmt = select(WarframeItemModel).limit(limit).offset(offset)
+
+    items = await session.execute(stmt)
+
+    return list(items.scalars().fetchall())
+
+
+@router.get(
+    "/{item_id}",
+    response_model=WarframeItemResponse,
+    response_description="A specific Warframe item",
+    status_code=status.HTTP_200_OK,
+)
+async def get_trusted_user(
+    session: Annotated[AsyncSession, Depends(get_db_session)],
+    item_id: str,
+) -> WarframeItemModel:
+    stmt = select(WarframeItemModel).where({WarframeItemModel.id == item_id})
+
+    items = await session.execute(stmt)
+
+    if (item := items.scalar_one_or_none()) is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            f"Item with ID {item_id} could not be found",
+        )
+
+    return item
--- api/app/routers/warframe/items/schemas.py
+++ api/app/routers/warframe/items/schemas.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
 
 
-class SyncResponse(BaseModel):
+class ItemsSyncResponse(BaseModel):
     response: str
+
+
+class WarframeItemResponse(BaseModel):
+    id: str
+    thumb: str
+    item_name: str
+    url_name: str
