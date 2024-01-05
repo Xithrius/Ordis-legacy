a058a7671d16cd7bc695852a3cd199b0b4936aef
Xithrius <xithrius@gmail.com>
2024-01-04 23:02:19 -0800

    Invite command, better README

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Ordis
 
 Cephalon Ordis from Warframe as a Discord bot.
-
-- prob about warframe market
-- or the time on earth
-- uh prob the incarnums and warframes in the circuit as well
-
-API Docs for Warframe can be found [here](https://docs.warframestat.us)
--- bot/bot/extensions/info/links.py
+++ bot/bot/extensions/info/links.py
@@ -1,11 +1,14 @@
-from discord import Embed
+from discord import Embed, Permissions
 from discord.ext.commands import Cog, command
+from discord.utils import oauth_url
 
 from bot.bot import Ordis
 from bot.context import Context
 from bot.utils import markdown_link
 
 GITHUB_URL = "https://github.com/Xithrius/Ordis"
+BOT_ID_INTEGER = 824131540912177162
+PERMISSIONS_INTEGER = 412320386112
 
 
 class Links(Cog):
@@ -18,6 +21,19 @@ class Links(Cog):
             description=markdown_link(
                 desc="Ordis Github Repository",
                 link=GITHUB_URL,
+            ),
+        )
+
+        await ctx.send(embed=embed)
+
+    @command()
+    async def invite(self, ctx: Context) -> None:
+        url = oauth_url(BOT_ID_INTEGER, permissions=Permissions(PERMISSIONS_INTEGER))
+
+        embed = Embed(
+            description=markdown_link(
+                desc="Invite link",
+                link=url,
             ),
         )
 
