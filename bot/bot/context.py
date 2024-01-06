34c16b9f507c7486739738489e8b034b741d4d1b
Xithrius <xithrius@gmail.com>
2024-01-05 23:00:28 -0800

    Removed send_buffer function from custom context

--- bot/bot/context.py
+++ bot/bot/context.py
@@ -1,16 +1,5 @@
-from io import BytesIO
-from typing import TYPE_CHECKING
-
-from discord import Embed
 from discord.ext.commands import Context as BaseContext
-
-if TYPE_CHECKING:
-    from bot.utils import send_image_buffer
 
 
 class Context(BaseContext):
     """Definition of a custom context."""
-
-    async def send_buffer(self, buffer: BytesIO, embed: Embed | None = None) -> None:
-        """Send the contents of a buffer as an image to a context."""
-        await send_image_buffer(buffer, ctx=self, embed=embed)
